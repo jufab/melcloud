6f8a07ccc37cbf7bf485bf0ca55c542fa0f39e5a
Julien FABRE <julien.fabre.33@gmail.com>
2017-08-20 03:09:10 +0200

    ajout du weather

---
+++ test/core/class/melcloudTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: julien
+ * Date: 15/08/2017
+ * Time: 22:28
+ */
+
+require_once dirname(__FILE__) . "/../../../core/class/melcloud.class.php";
+
+class melcloudTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetInfo()
+    {
+        $melcloudCmd = new melcloudCmd();
+        melcloud::obtenirInfo($melcloudCmd->getEqLogic());
+    }
+}
